d235bb79cac62b5f1cf6dd8c4c502dfbef11dddf
antoinechalifour <antoine.chalifour@gmail.com>
2018-06-24 13:28:19 +0200

    fix: Add delete party command handler

--- src/createCommandBus.js
+++ src/createCommandBus.js
@@ -2,11 +2,12 @@ const Dispatcher = require('./Commands/Dispatcher')
 const EventBusDispatcherMiddleware = require('./Commands/middlewares/EventDispatcher')
 const LoggerMiddleware = require('./Commands/middlewares/LoggerMiddleware')
 const CreatePartyHandler = require('./Commands/CreateParty/Handler')
+const DeletePartyHandler = require('./Commands/DeletePartyCommand/Handler')
 const JoinPartyHandler = require('./Commands/JoinParty/Handler')
 const LeavePartyHandler = require('./Commands/LeaveParty/Handler')
-const SendSignalingOfferHandler = require('./Commands/SendSignalingOffer/Handler')
 const SendSignalingAnswerHandler = require('./Commands/SendSignalingAnswer/Handler')
 const SendSignalingCandidateHandler = require('./Commands/SendSignalingCandidate/Handler')
+const SendSignalingOfferHandler = require('./Commands/SendSignalingOffer/Handler')
 
 module.exports = function createCommandBus ({
   eventBus,
@@ -16,11 +17,12 @@ module.exports = function createCommandBus ({
 }) {
   const handlers = [
     new CreatePartyHandler(userRepository, partyRepository),
+    new DeletePartyHandler(userRepository, partyRepository),
     new JoinPartyHandler(tokenService, partyRepository, userRepository),
     new LeavePartyHandler(userRepository),
-    new SendSignalingOfferHandler(userRepository),
     new SendSignalingAnswerHandler(userRepository),
-    new SendSignalingCandidateHandler(userRepository)
+    new SendSignalingCandidateHandler(userRepository),
+    new SendSignalingOfferHandler(userRepository)
   ]
   const bus = new LoggerMiddleware(
     new EventBusDispatcherMiddleware(new Dispatcher(handlers), eventBus),
